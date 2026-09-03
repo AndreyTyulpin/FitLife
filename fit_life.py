2c1de228c6aa5d6da57573b13098d40f564c59d4
Андрей Тюльпин <andrey.tyulpin@yandex.ru>
2026-09-03 16:09:49 +0300

    текст комментария

--- fit_life.py
+++ fit_life.py
@@ -2,17 +2,23 @@
 
 
 # 1. Знакомство
-# TODO: Спроси у пользователя имя и сохрани в переменную user_name
-# TODO: Спроси возраст и сохрани в переменную user_age
-user_name = input("Как вас зовут?")
+user_name = input("Как вас зовут?").strip()
 user_name = user_name.title()
 
 user_age = input("Сколько вам лет?")
 user_age = int(user_age)
+# Создаем переменную для окончания
+year = ""
 
+if 11 <= user_age % 100 <= 19:
+    year = "лет"
+elif user_age % 10 == 1:
+    year = "год"
+elif user_age % 10 in [2, 3, 4]:
+    year = "года"
+else:
+    year = "лет"
 # 2. Сбор данных
-# TODO: Запроси вес (в кг) и сохрани в user_weight (тип float)
-# TODO: Запроси рост (в метрах, например 1.75)
 user_weight = input("Скажите свой вес (в кг)?")
 user_weight = float(user_weight)
 
@@ -20,22 +26,17 @@ user_height = input("Скажите свой рост (в метрах)? Нап�
 user_height = float(user_height)
 
 
-# 3. Логика расчетов (Функции как "черный ящик": используем арифметику)
-# Формула ИМТ: вес разделить на (рост в квадрате)
-# TODO: Рассчитай bmi (Индекс массы тела)
+# 3. Рассчет bmi (Индекс массы тела)
 bmi = round(user_weight / (user_height ** 2), 1)
 
 # Подсчет воды: вес * 30 мл
-# TODO: Рассчитай water_needed
 water_ml = user_weight * 30
 water_needed = round((water_ml / 1000), 1)
 
 # 4. Вывод красивого результата
-# TODO: Используй f-строку, чтобы вывести приветствие
-# TODO: Выведи возраст, ИМТ (округленный до 1 знака) и норму воды.
 print(" " * 30)
-print(f"Отчет для пользователя: {user_name} {user_age} лет")
+print(f"Отчет для пользователя: {user_name} {user_age} {year}")
 print(f"Твой Индекс Массы Тела: {bmi}")
-print(f"Рекомендуемая норма воды: {water_needed} л. в день")
+print(f"Рекомендуемая норма воды: {water_needed} л в день")
 print(" " * 30)
 print("Расчет окончен. Будьте здоровы!")
